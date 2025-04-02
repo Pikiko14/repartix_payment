eafabcdd3ceca8f94496f8acef08f2401e3099d2
Marcos Hernandez <maalhequi95@gmail.com>
2025-04-02 10:49:57 -0500

    initial commit

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,22 @@
+import { RpcException } from '@nestjs/microservices';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { PaymentFactory } from './strategies/payment.factory';
+
+@Injectable()
+export class PaymentsService {
+  constructor() {}
+
+  async create(createPaymentDto: CreatePaymentDto) {
+    const paymentGateway = PaymentFactory.createPaymentGateway(createPaymentDto.paymentMethods);
+    try {
+      const paymentPreference = await paymentGateway.processPayment(createPaymentDto);
+      return paymentPreference;
+    } catch (error) {
+      throw new RpcException({
+        message: error.message,
+        status: HttpStatus.BAD_REQUEST,
+      })
+    }
+  }
+}
